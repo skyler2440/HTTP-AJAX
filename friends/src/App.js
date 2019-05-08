0e132b30a634b1d07c7d0d1021583fcdbd80315f
skyler dowdy <skylerwebdev@gmail.com>
2019-05-08 18:55:38 -0400

    what i have done so far

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,15 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+// import FriendDisplay from './components/FriendDisplay';
+import axios from "axios";
+import FriendDisplay from "./components/FriendDisplay";
+import { BrowserRouter as Route} from 'react';
+class App extends React.Component {
+  // add constructor and CDM
+  constructor() {
+    super();
+    this.state = {
+      friends: []
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-     
-      </header>
-    </div>
-  );
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/friends")
+      .then(res => {
+        console.log(res);
+        this.setState({ friends: res.data });
+        return axios.post("tehuihetuhs", res);
+      })
+      .then()
+      .catch(err => {
+        console.log(err);
+        this.setState({ error: err.response.message });
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {/* <FriendDisplay /> */}
+          <Route exact path="/" component={FriendDisplay} friends={this.state.friends} />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- friends/src/components/FriendDisplay.js
+++ friends/src/components/FriendDisplay.js
@@ -1,4 +1,9 @@
 import React from 'react';
-import axios from 'axios';
 
+function FriendDisplay(props) {
+return(
+    <p>{props.}</p>
+)
+}
 
+export default FriendDisplay;
